2bd7056d57a7864c605eafacfc0f199dc63e8d74
Ahmad Syarifuddin <oden.psht@gmail.com>
2025-08-05 10:42:17 +0700

    Setup Router

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/kryast/crud-11.git/database"
+	"github.com/kryast/crud-11.git/models"
+	"github.com/kryast/crud-11.git/router"
+)
+
+func Main() {
+	db, err := database.InitDB()
+	if err != nil {
+		log.Fatal("Failed to Connect Database", err)
+	}
+
+	db.AutoMigrate(&models.Feedback{})
+
+	r := router.SetupRouter(db)
+	r.Run(":8080")
+
+}
